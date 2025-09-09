e3df3d7ae9b7ec39f573797fab5a3d94fd115b0c
Prince Gupta <129318983+Monkey-d-prince@users.noreply.github.com>
2025-09-09 11:37:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    // increase the default warning limit (500kb → 1000kb)
-    chunkSizeWarningLimit: 3000,
-
-    rollupOptions: {
-      output: {
-        // Example: split vendor libs into a separate chunk
-        manualChunks: {
-          react: ['react', 'react-dom']
-        }
-      }
-    }
+// https://vite.dev/config/ 
+export default defineConfig(
+  {
+    plugins: [react()],
   }
-})
+)
